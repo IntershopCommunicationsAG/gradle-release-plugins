61cb865995f213c4f2a3f77aac695cb107285276
mraab <mraab@intershop.de>
2017-07-26 13:01:14 +0200

    - remove dependencies for setIssueField task - add task releaseLog - releaseLog depends on setIssueField only if the version does not end with snapshot

--- src/test/groovy/com/intershop/gradle/artifactorypublish/MultiProjectArtifactorySpec.groovy
+++ src/test/groovy/com/intershop/gradle/artifactorypublish/MultiProjectArtifactorySpec.groovy
@@ -22,6 +22,7 @@ import org.junit.Rule
 import spock.lang.Unroll
 
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+import static org.gradle.testkit.runner.TaskOutcome.UP_TO_DATE
 
 @Unroll
 class MultiProjectArtifactorySpec extends AbstractIntegrationSpec {
@@ -146,9 +147,90 @@ class MultiProjectArtifactorySpec extends AbstractIntegrationSpec {
         then:
         result.output.contains('CREATE JAVADOC')
         result.task(':artifactoryPublish').outcome == SUCCESS
-        result.task(':setIssueField').outcome == SUCCESS
+        result.getTasks().findAll( { it.path == ':setIssueField'} ).isEmpty()
+        //result.task(':setIssueField').outcome == SUCCESS
         upLoadListCheck
         upLoadList.size() == 4
+        //responses.get('onebody').contains('"project":{"key":"ISTOOLS"}')
+        //responses.get('onebody').contains('"issuetype":{"id":"10001"}')
+
+        where:
+        buildFileContent << [buildFileContentBase]
+    }
+
+    def 'test release log with artifactory configuration'() {
+        given:
+        String urlStr = server.url('/').toString()
+        List<String> upLoadList = []
+        Map<String,String> responses = [:]
+
+        server.setDispatcher(TestDispatcher.getIntegrationDispatcher(responses, upLoadList))
+
+        buildFile << """
+                plugins {
+                  id "ivy-publish"
+                  id 'com.intershop.gradle.scmversion' version '1.3.0'
+                  id 'com.intershop.gradle.buildinfo' version '2.0.0'
+                  id 'com.intershop.gradle.artifactorypublish-configuration'
+                }
+
+                group = 'com.intershop.testproject'
+                version = '1.0.0'
+
+                artifactory {
+                    publish {
+                        repository {
+                            maven = false
+                        }
+                        defaults {
+                            publications('ivy')
+                            properties = ['testBla': 'testBla']
+                        }
+                    }
+                }
+
+                subprojects {
+                    group = 'com.intershop.testproject'
+                    version = rootProject.getVersion()
+                }
+        """.stripIndent()
+
+        File settingsfile = file('settings.gradle')
+        settingsfile << """
+            // define root proejct name
+            rootProject.name = 'p_testProject'
+        """.stripIndent()
+        createSubProjectJava('project1a', settingsfile, 'com.intereshop.a', buildFileContent, '1.0.0')
+        createSubProjectJava('project2b', settingsfile, 'com.intereshop.b', buildFileContent, '1.0.0')
+
+        File changelog = file('build/changelog/changelog.asciidoc')
+        changelog << """
+        = Change Log for 2.0.0
+
+        This list contains changes since version 1.0.0. +
+        Created: Sun Feb 21 17:11:48 CET 2016
+
+        [cols="5%,5%,90%", width="95%", options="header"]
+        |===
+        3+| ${issueKey} change on master (e6c62c43)
+        | | M |  gradle.properties
+        3+| remove unnecessary files (a2da48ad)
+        | | D | gradle/wrapper/gradle-wrapper.jar
+        | | D | gradle/wrapper/gradle-wrapper.properties
+        |===""".stripIndent()
+
+        when:
+        def result = getPreparedGradleRunner()
+                .withArguments('releaseLog', '--exclude-task', 'changelog', '--stacktrace', '-i', "-DRUNONCI=true", '-PjiraFieldName=Labels', "-DARTIFACTORYBASEURL=${urlStr}", '-DSNAPSHOTREPOKEY=snapshots', '-DRELEASEREPOKEY=releases', '-DARTIFACTORYUSERNAME=admin', '-DARTIFACTORYUSERPASSWD=admin123', "-DJIRABASEURL=${urlStr}", '-DJIRAUSERNAME=admin', '-DJIRAUSERPASSWD=admin123')
+                .build()
+
+        boolean upLoadListCheck = true
+        upLoadList.each {
+            upLoadListCheck &= it.contains('releases/com.intershop.testproject/')
+        }
+
+        then:
+        result.task(':setIssueField').outcome == SUCCESS
         responses.get('onebody').contains('"project":{"key":"ISTOOLS"}')
         responses.get('onebody').contains('"issuetype":{"id":"10001"}')
 
@@ -395,12 +477,89 @@ class MultiProjectArtifactorySpec extends AbstractIntegrationSpec {
         upLoadListCheck
         upLoadList.size() == 4
         ! result.tasks.contains(':writeToJira')
-        upLoadListCheck
 
         where:
         buildFileContent << [buildFileContentBase]
     }
 
+    def 'test snapshot publishing with artifactory configuration and release log'() {
+        given:
+        String urlStr = server.url('/').toString()
+        List<String> upLoadList = []
+        Map<String,String> responses = [:]
+
+        server.setDispatcher(TestDispatcher.getIntegrationDispatcher(responses, upLoadList))
+
+        buildFile << """
+                plugins {
+                  id "ivy-publish"
+                  id 'com.intershop.gradle.scmversion' version '1.3.0'
+                  id 'com.intershop.gradle.buildinfo' version '2.0.0'
+                  id 'com.intershop.gradle.artifactorypublish-configuration'
+                }
+
+                group = 'com.intershop.testproject'
+                version = '1.0.0-SNAPSHOT'
+
+                artifactory {
+                    publish {
+                        repository {
+                            maven = false
+                        }
+                        defaults {
+                            publications('ivy')
+                        }
+                    }
+                }
+
+                subprojects {
+                    group = 'com.intershop.testproject'
+                    version = rootProject.getVersion()
+                }
+        """.stripIndent()
+
+        File settingsfile = file('settings.gradle')
+        settingsfile << """
+            // define root proejct name
+            rootProject.name = 'p_testProject'
+        """.stripIndent()
+        createSubProjectJava('project1a', settingsfile, 'com.intereshop.a', buildFileContent, '1.0.0-SNAPSHOT')
+        createSubProjectJava('project2b', settingsfile, 'com.intereshop.b', buildFileContent, '1.0.0-SNAPSHOT')
+
+        File changelog = file('project1a/build/changelog/changelog.asciidoc')
+        changelog << """
+        = Change Log for 2.0.0
+
+        This list contains changes since version 1.0.0. +
+        Created: Sun Feb 21 17:11:48 CET 2016
+
+        [cols="5%,5%,90%", width="95%", options="header"]
+        |===
+        3+| ${issueKey} change on master (e6c62c43)
+        | | M |  gradle.properties
+        3+| remove unnecessary files (a2da48ad)
+        | | D | gradle/wrapper/gradle-wrapper.jar
+        | | D | gradle/wrapper/gradle-wrapper.properties
+        |===""".stripIndent()
+
+        when:
+        def result = getPreparedGradleRunner()
+                .withArguments('releaseLog', '--exclude-task', 'changelog', '--stacktrace', '-i', "-DRUNONCI=true", '-PjiraFieldName=Labels', "-DARTIFACTORYBASEURL=${urlStr}", '-DSNAPSHOTREPOKEY=snapshots', '-DRELEASEREPOKEY=releases', '-DARTIFACTORYUSERNAME=admin', '-DARTIFACTORYUSERPASSWD=admin123', "-DJIRABASEURL=${urlStr}", '-DJIRAUSERNAME=admin', '-DJIRAUSERPASSWD=admin123', '-Dhttp.proxyHost=localhost', '-Dhttp.proxyPort=8200')
+                .build()
+
+        boolean upLoadListCheck = true
+        upLoadList.each {
+            upLoadListCheck &= it.contains('snapshots/com.intershop.testproject/')
+        }
+
+        then:
+        ! result.output.contains('CREATE JAVADOC')
+        result.task(':releaseLog').outcome == UP_TO_DATE
+        ! result.tasks.contains(':writeToJira')
+
+        where:
+        buildFileContent << [buildFileContentBase]
+    }
 
     /**
      * Creates a java sub project
--- src/test/groovy/com/intershop/gradle/artifactorypublish/SingleProjectSpec.groovy
+++ src/test/groovy/com/intershop/gradle/artifactorypublish/SingleProjectSpec.groovy
@@ -23,6 +23,7 @@ import com.squareup.okhttp.mockwebserver.MockWebServer
 import org.junit.Rule
 
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+import static org.gradle.testkit.runner.TaskOutcome.UP_TO_DATE
 
 class SingleProjectSpec extends AbstractIntegrationSpec {
 
@@ -107,8 +108,89 @@ class SingleProjectSpec extends AbstractIntegrationSpec {
         then:
         upLoadListCheck
         upLoadList.size() == 2
-        result.task(':setIssueField').outcome == SUCCESS
+        result.getTasks().findAll( { it.path == ':setIssueField'} ).isEmpty()
+        //result.task(':setIssueField').outcome == SUCCESS
         result.task(':artifactoryPublish').outcome == SUCCESS
+        //responses.get('onebody').contains('"project":{"key":"ISTOOLS"}')
+        //responses.get('onebody').contains('"issuetype":{"id":"10001"}')
+    }
+
+    def 'test release log with artifactory confioguration'() {
+        given:
+        String urlStr = server.url('/').toString()
+        List<String> upLoadList = []
+        Map<String,String> responses = [:]
+
+        File changelog = file('build/changelog/changelog.asciidoc')
+        changelog << """
+        = Change Log for 2.0.0
+
+        This list contains changes since version 1.0.0. +
+        Created: Sun Feb 21 17:11:48 CET 2016
+
+        [cols="5%,5%,90%", width="95%", options="header"]
+        |===
+        3+| ${issueKey} change on master (e6c62c43)
+        | | M |  gradle.properties
+        3+| remove unnecessary files (a2da48ad)
+        | | D | gradle/wrapper/gradle-wrapper.jar
+        | | D | gradle/wrapper/gradle-wrapper.properties
+        |===""".stripIndent()
+
+        server.setDispatcher(TestDispatcher.getIntegrationDispatcher(responses, upLoadList))
+
+        buildFile << """
+            plugins {
+                id 'java'
+                id 'ivy-publish'
+                id 'com.intershop.gradle.scmversion' version '1.3.0'
+                id 'com.intershop.gradle.buildinfo' version '2.0.0'
+                id 'com.intershop.gradle.artifactorypublish-configuration'
+            }
+
+            group = 'com.intershop'
+            version = '1.1.0'
+
+            artifactory {
+                publish {
+                    repository {
+                        maven = false
+                    }
+                    defaults {
+                        publications('ivy')
+                    }
+                }
+            }
+
+            publishing {
+                publications {
+                    ivy(IvyPublication) {
+                        from components.java
+                    }
+                }
+            }
+        """.stripIndent()
+
+        File settingsfile = file('settings.gradle')
+        settingsfile << """
+            // define root proejct name
+            rootProject.name = 'testProject'
+        """.stripIndent()
+        writeJavaTestClass("com.intershop.test")
+
+        when:
+        def result = getPreparedGradleRunner()
+                .withArguments('releaseLog', '--exclude-task', 'changelog', '--stacktrace', '-i', "-DRUNONCI=true", '-PjiraFieldName=Labels', "-DARTIFACTORYBASEURL=${urlStr}", '-DSNAPSHOTREPOKEY=snapshots', '-DRELEASEREPOKEY=releases', '-DARTIFACTORYUSERNAME=admin', '-DARTIFACTORYUSERPASSWD=admin123', "-DJIRABASEURL=${urlStr}", '-DJIRAUSERNAME=admin', '-DJIRAUSERPASSWD=admin123')
+                .build()
+
+        boolean upLoadListCheck = true
+        upLoadList.each {
+            upLoadListCheck &= it.contains('releases/com.intershop/')
+        }
+
+        then:
+        result.task(':setIssueField').outcome == SUCCESS
+        result.task(':releaseLog').outcome == SUCCESS
         responses.get('onebody').contains('"project":{"key":"ISTOOLS"}')
         responses.get('onebody').contains('"issuetype":{"id":"10001"}')
     }
@@ -186,10 +268,11 @@ class SingleProjectSpec extends AbstractIntegrationSpec {
         then:
         upLoadListCheck
         upLoadList.size() == 2
-        result.task(':setIssueField').outcome == SUCCESS
+        result.getTasks().findAll( { it.path == ':setIssueField'} ).isEmpty()
+        //result.task(':setIssueField').outcome == SUCCESS
         result.task(':artifactoryPublish').outcome == SUCCESS
-        responses.get('onebody').contains('"project":{"key":"ISTOOLS"}')
-        responses.get('onebody').contains('"issuetype":{"id":"10001"}')
+        //responses.get('onebody').contains('"project":{"key":"ISTOOLS"}')
+        //responses.get('onebody').contains('"issuetype":{"id":"10001"}')
     }
 
 
@@ -272,4 +355,79 @@ class SingleProjectSpec extends AbstractIntegrationSpec {
         ! result.tasks.contains(':writeToJira')
     }
 
+    def 'test snapshot release log with artifactory configuration'() {
+        given:
+        String urlStr = server.url('/').toString()
+        List<String> upLoadList = []
+        Map<String,String> responses = [:]
+
+        File changelog = file('build/changelog/changelog.asciidoc')
+        changelog << """
+        = Change Log for 2.0.0
+
+        This list contains changes since version 1.0.0. +
+        Created: Sun Feb 21 17:11:48 CET 2016
+
+        [cols="5%,5%,90%", width="95%", options="header"]
+        |===
+        3+| ${issueKey} change on master (e6c62c43)
+        | | M |  gradle.properties
+        3+| remove unnecessary files (a2da48ad)
+        | | D | gradle/wrapper/gradle-wrapper.jar
+        | | D | gradle/wrapper/gradle-wrapper.properties
+        |===""".stripIndent()
+
+        server.setDispatcher(TestDispatcher.getIntegrationDispatcher(responses, upLoadList))
+        buildFile << """
+            plugins {
+                id 'java'
+                id 'ivy-publish'
+                id 'com.intershop.gradle.scmversion' version '1.3.0'
+                id 'com.intershop.gradle.buildinfo' version '2.0.0'
+                id 'com.intershop.gradle.artifactorypublish-configuration'
+            }
+
+            group = 'com.intershop'
+            version = '1.0.0-SNAPSHOT'
+
+            artifactory {
+                publish {
+                    repository {
+                        maven = false
+                    }
+                    defaults {
+                        publications('ivy')
+                    }
+                }
+            }
+
+            publishing {
+                publications {
+                    ivy(IvyPublication) {
+                        from components.java
+                    }
+                }
+            }
+        """.stripIndent()
+
+        File settingsfile = file('settings.gradle')
+        settingsfile << """
+            // define root proejct name
+            rootProject.name = 'p_testProject'
+        """.stripIndent()
+        writeJavaTestClass("com.intershop.test")
+
+        when:
+        def result = getPreparedGradleRunner()
+                .withArguments('releaseLog', '--exclude-task', 'changelog', '--stacktrace', '-d', "-DRUNONCI=true", '-PjiraFieldName=Labels', "-DARTIFACTORYBASEURL=${urlStr}", '-DSNAPSHOTREPOKEY=snapshots', '-DRELEASEREPOKEY=releases', '-DARTIFACTORYUSERNAME=admin', '-DARTIFACTORYUSERPASSWD=admin123', "-DJIRABASEURL=${urlStr}", '-DJIRAUSERNAME=admin', '-DJIRAUSERPASSWD=admin123')
+                .build()
+
+        boolean upLoadListCheck = true
+        upLoadList.each {
+            upLoadListCheck &= it.contains('snapshots/com.intershop/')
+        }
+
+        then:
+        result.task(':releaseLog').outcome == UP_TO_DATE
+    }
 }
