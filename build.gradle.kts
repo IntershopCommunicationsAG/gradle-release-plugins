c9e9043104c2d0d0b100d42e87a2806a688a4f98
Matthias Raab <M.Raab@intershop.de>
2022-11-15 13:41:30 +0100

    set jdk compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,8 +92,8 @@ pluginBundle {
 
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
 }
 
 // set correct project status
